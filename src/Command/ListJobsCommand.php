3eb367c28a6028bf4ae34c0133c6c5d955c1c9e8
Filip Procházka <filip@prochazka.su>
2017-05-12 00:56:23 +0200

    list-jobs command

---
+++ src/Command/ListJobsCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Fprochazka\TravisLocalBuild\Command;
+
+use Fprochazka\TravisLocalBuild\BuildMatrix;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * @author Filip Procházka <filip@prochazka.su>
+ */
+class ListJobsCommand extends \Symfony\Component\Console\Command\Command
+{
+
+	protected function configure()
+	{
+		$this->setName('list-jobs');
+	}
+
+	public function run(InputInterface $input, OutputInterface $output): int
+	{
+		$buildMatrix = new BuildMatrix($output);
+		$jobs = $buildMatrix->create(getcwd());
+
+		$output->writeln(sprintf('Found %d jobs:', count($jobs)));
+		foreach ($jobs as $job) {
+			$output->writeln(sprintf('- php:%s %s', $job->getPhpVersion(), $job->getEnvLine()));
+		}
+		$output->writeln('');
+
+		return 0;
+	}
+
+}
