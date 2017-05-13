cad00aac6cc9288292081ac9908cdcdfb4143972
Filip Procházka <filip@prochazka.su>
2017-05-13 17:04:46 +0200

    refactor tests

--- tests/Travis/BuildMatrixTest.php
+++ tests/Travis/BuildMatrixTest.php
@@ -26,37 +26,47 @@ class BuildMatrixTest extends TestCase
 		$jobs = $this->matrix->computeJobs('consistence/consistence', __DIR__, $this->parseConfig(__DIR__ . '/../configs/consistence.yml'));
 		$this->assertCount(8, $jobs);
 
-		$this->assertEquals('7.0', $jobs[0]->getPhpVersion());
-		$this->assertEquals('', $jobs[0]->getEnvLine());
-		$this->assertFalse($jobs[0]->isAllowedFailure());
+		$job = array_shift($jobs);
+		$this->assertEquals('7.0', $job->getPhpVersion());
+		$this->assertEquals('', $job->getEnvLine());
+		$this->assertFalse($job->isAllowedFailure());
 
-		$this->assertEquals('7.0', $jobs[1]->getPhpVersion());
-		$this->assertEquals('COMPOSER_DEPENDENCIES_OPTIONS="--prefer-lowest --prefer-stable"', $jobs[1]->getEnvLine());
-		$this->assertFalse($jobs[1]->isAllowedFailure());
+		$job = array_shift($jobs);
+		$this->assertEquals('7.0', $job->getPhpVersion());
+		$this->assertEquals('COMPOSER_DEPENDENCIES_OPTIONS="--prefer-lowest --prefer-stable"', $job->getEnvLine());
+		$this->assertFalse($job->isAllowedFailure());
 
-		$this->assertEquals('7.1', $jobs[2]->getPhpVersion());
-		$this->assertEquals('', $jobs[2]->getEnvLine());
-		$this->assertFalse($jobs[2]->isAllowedFailure());
+		$job = array_shift($jobs);
+		$this->assertEquals('7.1', $job->getPhpVersion());
+		$this->assertEquals('', $job->getEnvLine());
+		$this->assertFalse($job->isAllowedFailure());
 
-		$this->assertEquals('7.1', $jobs[3]->getPhpVersion());
-		$this->assertEquals('COMPOSER_DEPENDENCIES_OPTIONS="--prefer-lowest --prefer-stable"', $jobs[3]->getEnvLine());
-		$this->assertFalse($jobs[3]->isAllowedFailure());
+		$job = array_shift($jobs);
+		$this->assertEquals('7.1', $job->getPhpVersion());
+		$this->assertEquals('COMPOSER_DEPENDENCIES_OPTIONS="--prefer-lowest --prefer-stable"', $job->getEnvLine());
+		$this->assertFalse($job->isAllowedFailure());
 
-		$this->assertEquals('hhvm', $jobs[4]->getPhpVersion());
-		$this->assertEquals('', $jobs[4]->getEnvLine());
-		$this->assertTrue($jobs[4]->isAllowedFailure());
+		$job = array_shift($jobs);
+		$this->assertEquals('hhvm', $job->getPhpVersion());
+		$this->assertEquals('', $job->getEnvLine());
+		$this->assertTrue($job->isAllowedFailure());
 
-		$this->assertEquals('hhvm', $jobs[5]->getPhpVersion());
-		$this->assertEquals('COMPOSER_DEPENDENCIES_OPTIONS="--prefer-lowest --prefer-stable"', $jobs[5]->getEnvLine());
-		$this->assertTrue($jobs[5]->isAllowedFailure());
+		$job = array_shift($jobs);
+		$this->assertEquals('hhvm', $job->getPhpVersion());
+		$this->assertEquals('COMPOSER_DEPENDENCIES_OPTIONS="--prefer-lowest --prefer-stable"', $job->getEnvLine());
+		$this->assertTrue($job->isAllowedFailure());
 
-		$this->assertEquals('nightly', $jobs[6]->getPhpVersion());
-		$this->assertEquals('', $jobs[6]->getEnvLine());
-		$this->assertTrue($jobs[6]->isAllowedFailure());
+		$job = array_shift($jobs);
+		$this->assertEquals('nightly', $job->getPhpVersion());
+		$this->assertEquals('', $job->getEnvLine());
+		$this->assertTrue($job->isAllowedFailure());
 
-		$this->assertEquals('nightly', $jobs[7]->getPhpVersion());
-		$this->assertEquals('COMPOSER_DEPENDENCIES_OPTIONS="--prefer-lowest --prefer-stable"', $jobs[7]->getEnvLine());
-		$this->assertTrue($jobs[7]->isAllowedFailure());
+		$job = array_shift($jobs);
+		$this->assertEquals('nightly', $job->getPhpVersion());
+		$this->assertEquals('COMPOSER_DEPENDENCIES_OPTIONS="--prefer-lowest --prefer-stable"', $job->getEnvLine());
+		$this->assertTrue($job->isAllowedFailure());
+
+		$this->assertEmpty($jobs);
 	}
 
 	public function testKdybyAnnotations()
@@ -64,45 +74,57 @@ class BuildMatrixTest extends TestCase
 		$jobs = $this->matrix->computeJobs('kdyby/annotations', __DIR__, $this->parseConfig(__DIR__ . '/../configs/kdyby-annotations.yml'));
 		$this->assertCount(10, $jobs);
 
-		$this->assertEquals('5.4', $jobs[0]->getPhpVersion());
-		$this->assertEquals('NETTE=nette-2.3', $jobs[0]->getEnvLine());
-		$this->assertFalse($jobs[0]->isAllowedFailure());
+		$job = array_shift($jobs);
+		$this->assertEquals('5.4', $job->getPhpVersion());
+		$this->assertEquals('NETTE=nette-2.3', $job->getEnvLine());
+		$this->assertFalse($job->isAllowedFailure());
 
-		$this->assertEquals('5.5', $jobs[1]->getPhpVersion());
-		$this->assertEquals('NETTE=nette-2.3', $jobs[1]->getEnvLine());
-		$this->assertFalse($jobs[1]->isAllowedFailure());
+		$job = array_shift($jobs);
+		$this->assertEquals('5.5', $job->getPhpVersion());
+		$this->assertEquals('NETTE=nette-2.3', $job->getEnvLine());
+		$this->assertFalse($job->isAllowedFailure());
 
-		$this->assertEquals('5.6', $jobs[2]->getPhpVersion());
-		$this->assertEquals('NETTE=nette-2.4-dev', $jobs[2]->getEnvLine());
-		$this->assertFalse($jobs[2]->isAllowedFailure());
+		$job = array_shift($jobs);
+		$this->assertEquals('5.6', $job->getPhpVersion());
+		$this->assertEquals('NETTE=nette-2.4-dev', $job->getEnvLine());
+		$this->assertFalse($job->isAllowedFailure());
 
-		$this->assertEquals('5.6', $jobs[3]->getPhpVersion());
-		$this->assertEquals('NETTE=nette-2.4', $jobs[3]->getEnvLine());
-		$this->assertFalse($jobs[3]->isAllowedFailure());
+		$job = array_shift($jobs);
+		$this->assertEquals('5.6', $job->getPhpVersion());
+		$this->assertEquals('NETTE=nette-2.4', $job->getEnvLine());
+		$this->assertFalse($job->isAllowedFailure());
 
-		$this->assertEquals('5.6', $jobs[4]->getPhpVersion());
-		$this->assertEquals('NETTE=nette-2.3', $jobs[4]->getEnvLine());
-		$this->assertFalse($jobs[4]->isAllowedFailure());
+		$job = array_shift($jobs);
+		$this->assertEquals('5.6', $job->getPhpVersion());
+		$this->assertEquals('NETTE=nette-2.3', $job->getEnvLine());
+		$this->assertFalse($job->isAllowedFailure());
 
-		$this->assertEquals('5.6', $jobs[5]->getPhpVersion());
-		$this->assertEquals('NETTE=nette-2.3 COMPOSER_EXTRA_ARGS="--prefer-lowest --prefer-stable"', $jobs[5]->getEnvLine());
-		$this->assertFalse($jobs[5]->isAllowedFailure());
+		$job = array_shift($jobs);
+		$this->assertEquals('5.6', $job->getPhpVersion());
+		$this->assertEquals('NETTE=nette-2.3 COMPOSER_EXTRA_ARGS="--prefer-lowest --prefer-stable"', $job->getEnvLine());
+		$this->assertFalse($job->isAllowedFailure());
 
-		$this->assertEquals('7.0', $jobs[6]->getPhpVersion());
-		$this->assertEquals('NETTE=nette-2.4-dev', $jobs[6]->getEnvLine());
-		$this->assertFalse($jobs[6]->isAllowedFailure());
+		$job = array_shift($jobs);
+		$this->assertEquals('7.0', $job->getPhpVersion());
+		$this->assertEquals('NETTE=nette-2.4-dev', $job->getEnvLine());
+		$this->assertFalse($job->isAllowedFailure());
 
-		$this->assertEquals('7.0', $jobs[7]->getPhpVersion());
-		$this->assertEquals('NETTE=nette-2.4', $jobs[7]->getEnvLine());
-		$this->assertFalse($jobs[7]->isAllowedFailure());
+		$job = array_shift($jobs);
+		$this->assertEquals('7.0', $job->getPhpVersion());
+		$this->assertEquals('NETTE=nette-2.4', $job->getEnvLine());
+		$this->assertFalse($job->isAllowedFailure());
 
-		$this->assertEquals('7.0', $jobs[8]->getPhpVersion());
-		$this->assertEquals('NETTE=nette-2.3', $jobs[8]->getEnvLine());
-		$this->assertFalse($jobs[8]->isAllowedFailure());
+		$job = array_shift($jobs);
+		$this->assertEquals('7.0', $job->getPhpVersion());
+		$this->assertEquals('NETTE=nette-2.3', $job->getEnvLine());
+		$this->assertFalse($job->isAllowedFailure());
 
-		$this->assertEquals('7.0', $jobs[9]->getPhpVersion());
-		$this->assertEquals('NETTE=nette-2.4 COVERAGE="--coverage ./coverage.xml --coverage-src ./src" TESTER_RUNTIME="phpdbg"', $jobs[9]->getEnvLine());
-		$this->assertTrue($jobs[9]->isAllowedFailure());
+		$job = array_shift($jobs);
+		$this->assertEquals('7.0', $job->getPhpVersion());
+		$this->assertEquals('NETTE=nette-2.4 COVERAGE="--coverage ./coverage.xml --coverage-src ./src" TESTER_RUNTIME="phpdbg"', $job->getEnvLine());
+		$this->assertTrue($job->isAllowedFailure());
+
+		$this->assertEmpty($jobs);
 	}
 
 	public function testKdybyDoctrineCache()
@@ -110,45 +132,57 @@ class BuildMatrixTest extends TestCase
 		$jobs = $this->matrix->computeJobs('kdyby/doctrine-cache', __DIR__, $this->parseConfig(__DIR__ . '/../configs/kdyby-doctrine-cache.yml'));
 		$this->assertCount(10, $jobs);
 
-		$this->assertEquals('5.6', $jobs[0]->getPhpVersion());
-		$this->assertEquals('', $jobs[0]->getEnvLine());
-		$this->assertTrue($jobs[0]->isAllowedFailure());
+		$job = array_shift($jobs);
+		$this->assertEquals('5.6', $job->getPhpVersion());
+		$this->assertEquals('', $job->getEnvLine());
+		$this->assertTrue($job->isAllowedFailure());
 
-		$this->assertEquals('5.6', $jobs[1]->getPhpVersion());
-		$this->assertEquals('COMPOSER_EXTRA_ARGS="--prefer-stable"', $jobs[1]->getEnvLine());
-		$this->assertFalse($jobs[1]->isAllowedFailure());
+		$job = array_shift($jobs);
+		$this->assertEquals('5.6', $job->getPhpVersion());
+		$this->assertEquals('COMPOSER_EXTRA_ARGS="--prefer-stable"', $job->getEnvLine());
+		$this->assertFalse($job->isAllowedFailure());
 
-		$this->assertEquals('5.6', $jobs[2]->getPhpVersion());
-		$this->assertEquals('COMPOSER_EXTRA_ARGS="--prefer-lowest --prefer-stable"', $jobs[2]->getEnvLine());
-		$this->assertFalse($jobs[2]->isAllowedFailure());
+		$job = array_shift($jobs);
+		$this->assertEquals('5.6', $job->getPhpVersion());
+		$this->assertEquals('COMPOSER_EXTRA_ARGS="--prefer-lowest --prefer-stable"', $job->getEnvLine());
+		$this->assertFalse($job->isAllowedFailure());
 
-		$this->assertEquals('7.0', $jobs[3]->getPhpVersion());
-		$this->assertEquals('', $jobs[3]->getEnvLine());
-		$this->assertTrue($jobs[3]->isAllowedFailure());
+		$job = array_shift($jobs);
+		$this->assertEquals('7.0', $job->getPhpVersion());
+		$this->assertEquals('', $job->getEnvLine());
+		$this->assertTrue($job->isAllowedFailure());
 
-		$this->assertEquals('7.0', $jobs[4]->getPhpVersion());
-		$this->assertEquals('COMPOSER_EXTRA_ARGS="--prefer-stable"', $jobs[4]->getEnvLine());
-		$this->assertFalse($jobs[4]->isAllowedFailure());
+		$job = array_shift($jobs);
+		$this->assertEquals('7.0', $job->getPhpVersion());
+		$this->assertEquals('COMPOSER_EXTRA_ARGS="--prefer-stable"', $job->getEnvLine());
+		$this->assertFalse($job->isAllowedFailure());
 
-		$this->assertEquals('7.0', $jobs[5]->getPhpVersion());
-		$this->assertEquals('COMPOSER_EXTRA_ARGS="--prefer-lowest --prefer-stable"', $jobs[5]->getEnvLine());
-		$this->assertFalse($jobs[5]->isAllowedFailure());
+		$job = array_shift($jobs);
+		$this->assertEquals('7.0', $job->getPhpVersion());
+		$this->assertEquals('COMPOSER_EXTRA_ARGS="--prefer-lowest --prefer-stable"', $job->getEnvLine());
+		$this->assertFalse($job->isAllowedFailure());
 
-		$this->assertEquals('7.1', $jobs[6]->getPhpVersion());
-		$this->assertEquals('', $jobs[6]->getEnvLine());
-		$this->assertTrue($jobs[6]->isAllowedFailure());
+		$job = array_shift($jobs);
+		$this->assertEquals('7.1', $job->getPhpVersion());
+		$this->assertEquals('', $job->getEnvLine());
+		$this->assertTrue($job->isAllowedFailure());
 
-		$this->assertEquals('7.1', $jobs[7]->getPhpVersion());
-		$this->assertEquals('COMPOSER_EXTRA_ARGS="--prefer-stable"', $jobs[7]->getEnvLine());
-		$this->assertFalse($jobs[7]->isAllowedFailure());
+		$job = array_shift($jobs);
+		$this->assertEquals('7.1', $job->getPhpVersion());
+		$this->assertEquals('COMPOSER_EXTRA_ARGS="--prefer-stable"', $job->getEnvLine());
+		$this->assertFalse($job->isAllowedFailure());
 
-		$this->assertEquals('7.1', $jobs[8]->getPhpVersion());
-		$this->assertEquals('COMPOSER_EXTRA_ARGS="--prefer-lowest --prefer-stable"', $jobs[8]->getEnvLine());
-		$this->assertFalse($jobs[8]->isAllowedFailure());
+		$job = array_shift($jobs);
+		$this->assertEquals('7.1', $job->getPhpVersion());
+		$this->assertEquals('COMPOSER_EXTRA_ARGS="--prefer-lowest --prefer-stable"', $job->getEnvLine());
+		$this->assertFalse($job->isAllowedFailure());
 
-		$this->assertEquals('7.1', $jobs[9]->getPhpVersion());
-		$this->assertEquals('COMPOSER_EXTRA_ARGS="--prefer-stable" COVERAGE="--coverage ./coverage.xml --coverage-src ./src" TESTER_RUNTIME="phpdbg"', $jobs[9]->getEnvLine());
-		$this->assertTrue($jobs[9]->isAllowedFailure());
+		$job = array_shift($jobs);
+		$this->assertEquals('7.1', $job->getPhpVersion());
+		$this->assertEquals('COMPOSER_EXTRA_ARGS="--prefer-stable" COVERAGE="--coverage ./coverage.xml --coverage-src ./src" TESTER_RUNTIME="phpdbg"', $job->getEnvLine());
+		$this->assertTrue($job->isAllowedFailure());
+
+		$this->assertEmpty($jobs);
 	}
 
 	private function parseConfig(string $config): array
