360bc3dc6715c7ebe4f7a291f39ba8e0853aa676
Filip Procházka <filip@prochazka.su>
2017-07-22 19:55:12 +0200

    list jobs as executable commands

--- src/Command/ListJobsCommand.php
+++ src/Command/ListJobsCommand.php
@@ -6,6 +6,7 @@ namespace Fprochazka\TravisLocalBuild\Command;
 
 use Fprochazka\TravisLocalBuild\Travis\BuildMatrix;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
@@ -14,10 +15,13 @@ use Symfony\Component\Console\Output\OutputInterface;
 class ListJobsCommand extends \Symfony\Component\Console\Command\Command
 {
 
+	const EXECUTABLE_OPTION = 'executable';
+
 	protected function configure()
 	{
 		$this->setName('list-jobs');
 		$this->setDescription('Outputs the list of Travis jobs. If it\'s red, it\'s allowed to fail (on Travis, not here)');
+		$this->addOption(self::EXECUTABLE_OPTION, 'e', InputOption::VALUE_NONE, 'List jobs as executable commands');
 	}
 
 	public function execute(InputInterface $input, OutputInterface $output): int
@@ -27,7 +31,12 @@ class ListJobsCommand extends \Symfony\Component\Console\Command\Command
 
 		$output->writeln(sprintf('Found %d jobs:', count($jobs)));
 		foreach ($jobs as $job) {
-			$output->writeln(sprintf('* <%s>php:%s %s</>', $job->isAllowedFailure() ? 'fg=red' : 'info', $job->getPhpVersion(), $job->getEnvLine()));
+			if ($input->getOption(self::EXECUTABLE_OPTION)) {
+				$output->writeln(sprintf('* <%s>travis-local run --php %s --env \'%s\'</>', $job->isAllowedFailure() ? 'fg=red' : 'info', $job->getPhpVersion(), $job->getEnvLine()));
+
+			} else {
+				$output->writeln(sprintf('* <%s>php:%s %s</>', $job->isAllowedFailure() ? 'fg=red' : 'info', $job->getPhpVersion(), $job->getEnvLine()));
+			}
 		}
 		$output->writeln('');
 
